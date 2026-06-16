e3b5b5e6f22b04d57d1abf43bd34b33de7d02455
Chloé Adonon <99622570+C-adonon@users.noreply.github.com>
2026-06-16 03:14:25 +0200

    feat: :lipstick: added primevue theme & options

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
 import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+import { definePreset } from '@primeuix/themes'
+import './assets/styles/main.css'
 
 import App from './App.vue'
 import router from './router'
@@ -9,6 +13,17 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(PrimeVue)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: false,
+      cssLayer: {
+        name: 'primevue',
+        order: 'theme, base, primevue',
+      },
+    },
+  },
+})
 
 app.mount('#app')
