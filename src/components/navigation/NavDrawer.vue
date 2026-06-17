e705a2e2758c7b9c9a917d153ffb551e805b117f
Chloé Adonon <99622570+C-adonon@users.noreply.github.com>
2026-06-17 03:17:25 +0200

    feat: :sparkles: init drawer (nav) component

---
+++ src/components/navigation/NavDrawer.vue
@@ -0,0 +1,13 @@
+<script setup>
+import SideBar from '@/components/navigation/SideBar.vue'
+const visible = defineModel({ type: Boolean, default: false })
+</script>
+
+<template>
+  <Drawer v-model:visible="visible">
+    <template #container>
+        <Button icon="pi pi-times" class="p-button-rounded p-button-text p-button-plain absolute top-2 right-2" @click="visible = false" />
+      <SideBar />
+    </template>
+  </Drawer>
+</template>
