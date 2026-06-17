be4fc0eed3e9d99cd1d929f47335be466f420a6c
Chloé Adonon <99622570+C-adonon@users.noreply.github.com>
2026-06-17 03:12:44 +0200

    feat: :sparkles: init layout

---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue'
+import { RouterView } from 'vue-router'
+import SideBar from '@/components/navigation/SideBar.vue'
+import NavDrawer from '@/components/navigation/NavDrawer.vue'
+import NavBar from '@/components/navigation/NavBar.vue'
+
+const isDrawerOpen = ref(false)
+</script>
+
+<template>
+  <div class="app-layout">
+    <aside class="sidebar hidden md:block">
+      <SideBar />
+    </aside>
+    <section class="app-content">
+      <RouterView />
+    </section>
+      <NavDrawer v-model="isDrawerOpen" />
+    <section class="app-navbar block md:hidden">
+      <NavBar @open-sideBar="isDrawerOpen = true" />
+    </section>
+  </div>
+</template>
